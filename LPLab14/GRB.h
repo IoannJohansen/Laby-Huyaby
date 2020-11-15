f30b1764ff4cc71767e04074b32499cbde74084d
John Heavy <boss.vanya20@mail.ru>
2020-11-15 19:56:50 +0300

    Lab 16 copmleted

--- LPLab14/GRB.h
+++ LPLab14/GRB.h
@@ -3,8 +3,9 @@
 typedef short GRBALPHABET;
 namespace GRB
 {
-#define NS(n) Rule::Chain::N(n)			//	
-#define TS(n) Rule::Chain::T(n)			//	
+#define NS(n) Rule::Chain::N(n)			//	NT
+#define TS(n) Rule::Chain::T(n)			//	T
+
 	struct Rule				// правило в грамматике Грейбах
 	{
 		GRBALPHABET nn;		// нетерминал(левый символ правила) <0
@@ -25,13 +26,16 @@ namespace GRB
 			static char alphabet_to_char(GRBALPHABET s) { return isT(s) ? char(s) : char(-s); }	//GRBALPHABET->char
 		} *chains;										//массив цепочек - правых частей правила
 		Rule() { nn = 0x00; size = 0; }
+		
 		Rule(GRBALPHABET pnn,						//нетерминал (<0)
 			int iderror,							//идентификатор диагностического сообщения
 			short psize,							//количество цепочек - правых частей правила
 			Chain c, ...);							//множество цепочек - правых частей правила
+
 		char* getCRule(								//получить правило в виде N->цепочка(для распечатки)
 			char* b,								//буфер
 			short nchain);							//номер цепочки(правой части) в правиле
+		
 		short getNextChain(							//получить следующую за j подходящую цепочку, вернуть её номер или -1
 			GRBALPHABET t,							//первый символ цепочки
 			Rule::Chain& pchain,					//возвращаемая цепочка
@@ -44,15 +48,19 @@ namespace GRB
 		GRBALPHABET startN;							 //стартовый символ
 		GRBALPHABET stbottomT;						//дно стека
 		Rule* rules;								 //множество правил
+
 		Greibach() { size = 0; startN = 0; stbottomT = 0; rules = 0; }
+
 		Greibach(
 			GRBALPHABET pstartN,					//стартовый символ
 			GRBALPHABET pstbottomT,					//дно стека
 			short psize,							//количество правил
 			Rule r, ...);							//правила
+
 		short getRule(								//получить правило, возвращается номер правила или -1
 			GRBALPHABET pnn,						//левый символ правила
 			Rule& prule);							//возвращаемое правило грамматики
+
 		Rule getRule(short n);						//получить правило по номеру
 	};
 
--- LPLab14/PolishNotation.cpp
+++ LPLab14/PolishNotation.cpp
@@ -16,7 +16,7 @@ namespace Polska
 	bool PolishNotation(int lexPos, LT::LexTable& lexTable, IT::IdTable& idenTable)
 	{
 		//DETECKTING ERRORS
-		if (lexTable.table[lexPos].lexema == LEX_ID && lexTable.table[lexPos + 1].lexema == LEX_LEFTHESIS)return false;	// we dont checking expressions with functions
+		if (lexTable.table[lexPos].lexema == LEX_ID && lexTable.table[lexPos + 1].lexema == LEX_LEFTHESIS && lexTable.table[lexPos + 2].lexema != LEX_ARITHMETIC && lexTable.table[lexPos + 2].lexema != LEX_RIGHTHESIS)return false;	// we dont checking expressions with functions
 		if ((lexTable.table[lexPos].lexema == LEX_ID|| lexTable.table[lexPos].lexema == LEX_LITERAL)&& lexTable.table[lexPos + 1].lexema == LEX_SEMICOLON)return false;
 		if (lexTable.table[lexPos].lexema == LEX_ARITHMETIC && idenTable.table[lexTable.table[lexPos].idxTI].id[0]!=LEX_MINUS)throw ERROR_THROW(139);
 		//DETECKTING ERRORS
@@ -32,6 +32,8 @@ namespace Polska
 			semicolonId = i + 1;
 			if ((lexTable.table[i].lexema == LEX_ID) && (lexTable.table[i + 1].lexema == LEX_LEFTHESIS))throw ERROR_THROW(141);	 //----!!!!!!
 			if (lexTable.table[i].lexema == LEX_ARITHMETIC && lexTable.table[i - 1].lexema == LEX_ARITHMETIC)throw ERROR_THROW(142);	 // bad interchange of operand - operation
+			if ((lexTable.table[i].lexema == LEX_ID) && (lexTable.table[i - 1].lexema == LEX_RIGHTHESIS))throw ERROR_THROW(143);	//---LAST
+			if ((lexTable.table[i].lexema == LEX_ARITHMETIC) && (lexTable.table[i-1].lexema == LEX_LEFTHESIS) && (lexTable.table[i + 1].lexema == LEX_RIGHTHESIS))throw ERROR_THROW(144);			//------!
 		}
 		srcLen++;					// чтоб выводилась ;
 
@@ -41,7 +43,7 @@ namespace Polska
 			switch (lexTable.table[i].lexema)
 			{
 			case LEX_LITERAL:	//ЫАЫАЫАЫАЫЫАЫАЫЫЫЫЫЫЫЫЫЫЫЫААААААААЫАЫАЫАЫАЫАЫАЫАЫАЫАЫЫАЫАЫАЫАЫАЫАЫЫАЫАЫЫЫЫЫЫЫЫЫЫЫЫААААААААЫАЫАЫАЫАЫАЫАЫАЫАЫАЫЫАЫА
-			case LEX_ID:	//операнды переносятся в результирующую строку в порядке их следования
+			case LEX_ID:
 			{
 				countOperands++;
 				resStr[outLen++] = lexTable.table[i];
@@ -50,7 +52,7 @@ namespace Polska
 			case LEX_ARITHMETIC:
 			{
 				countOperations++;
-				if (stack.empty() || stack.top().lexema == LEX_LEFTHESIS)// операция записывается в стек, если стек пуст или в вершине стека лежит открывающая скобка	
+				if (stack.empty() || stack.top().lexema == LEX_LEFTHESIS)
 				{
 					stack.push(lexTable.table[i]);
 					break;
@@ -58,7 +60,7 @@ namespace Polska
 
 
 				while ((ArifmPriorities(stack.top(), idenTable) >= ArifmPriorities(lexTable.table[i], idenTable)))
-				{		// операция выталкивает все операции с большим или равным приоритетом в результирующую строку
+				{
 					resStr[outLen++] = stack.top();
 					stack.pop();
 					if (stack.empty())break;		// -!!!
@@ -68,7 +70,7 @@ namespace Polska
 			}
 			
 
-			case LEX_LEFTHESIS:		//  открывающая скобка помещается в стек
+			case LEX_LEFTHESIS:	
 			{
 				if (lexTable.table[i + 1].lexema == LEX_RIGHTHESIS)throw ERROR_THROW(137);
 				brBalance++;
@@ -76,7 +78,7 @@ namespace Polska
 				break;
 			}
 
-			case LEX_RIGHTHESIS:	// закрывающая скобка выталкивает все операции до открывающей скобки, после чего обе скобки уничтожаются
+			case LEX_RIGHTHESIS:
 			{
 				if (brBalance <= 0)throw ERROR_THROW(138);
 				brBalance--;
@@ -88,7 +90,7 @@ namespace Polska
 				stack.pop();
 				break;
 			}
-			case LEX_SEMICOLON:break;		// bas symbol in expression
+			case LEX_SEMICOLON:break;		// bad symbol in expression
 
 			default:						// UNCKNOWED SYMBOLS IN EXPRESSION
 			{
@@ -97,7 +99,7 @@ namespace Polska
 			}
 			}
 
-			if ((counter==srcLen)&&(!stack.empty()))	// по концу разбора исходной строки все операции, оставшиеся в стеке, выталкиваются в результирующую строку
+			if ((counter==srcLen)&&(!stack.empty()))	
 			{
 				while (!stack.empty())
 				{
